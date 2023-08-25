975a68323bebf44ef233195c4d97f33da19390ee
reflexson <131407662+reflexson@users.noreply.github.com>
2023-08-25 10:42:22 -0500

    handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -12,7 +12,7 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/dashboard');
+      document.location.replace('/allprojects');
     } else {
       alert(response.statusText);
     }
