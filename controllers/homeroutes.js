e8688c0c56092ce4345a98d9879ca6a2f9499eec
abstractl0gic <zwlyons@gmail.com>
2023-08-24 21:27:05 -0500

    modify homeroutes to use song

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,97 +1,69 @@
 const router = require('express').Router();
-const { User, Blog, Comment } = require('../models');
+const { User, Song, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-	try {
-		const blogData = await Blog.findAll({
-			include: [{
-				model: User,
-				attributes: ['username'],
-			},],
-		});
+  try {
+    const songData = await Song.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
 
-		const blogs = blogData.map((blog) => blog.get({
-			plain: true
-		}));
+    const songs = songData.map((song) => song.get({ plain: true }));
 
-		res.render('homepage', {
-			blogs,
-			logged_in: req.session.logged_in
-		});
-	} catch (err) {
-		res.status(500).json(err);
-	}
-});
-
-router.get('/blog/:id', async (req, res) => {
-	try {
-		const blogData = await Blog.findByPk(req.params.id, {
-			include: [
-				{
-					model: User,
-					attributes: ['username'],
-				}, {
-					model: Comment,
-					include: [
-						User
-					]
-				}
-			],
-		});
-
-		const blog = blogData.get({
-			plain: true
-		});
-
-		res.render('blog', {
-			...blog,
-			logged_in: req.session.logged_in
-		});
-	} catch (err) {
-		res.status(500).json(err);
-	}
+    res.render('homepage', {
+      songs,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/dashboard', withAuth, async (req, res) => {
-	try {
-		const userData = await User.findByPk(req.session.user_id, {
-			attributes: {
-				exclude: ['password']
-			},
-			include: [{
-				model: Blog
-			}],
-		});
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: {
+        exclude: ['password'],
+      },
+      include: [
+        {
+          model: Song,
+          include: [Comment], 
+        },
+      ],
+    });
 
-		const user = userData.get({
-			plain: true
-		});
+    const user = userData.get({ plain: true });
 
-		res.render('dashboard', {
-			...user,
-			logged_in: true
-		});
-	} catch (err) {
-		res.status(500).json(err);
-	}
+    res.render('dashboard', {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/login', (req, res) => {
-	if (req.session.logged_in) {
-		res.redirect('/dashboard');
-		return;
-	}
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
 
-	res.render('login');
+  res.render('login');
 });
 
 router.get('/signUp', (req, res) => {
-	if (req.session.logged_in) {
-		res.redirect('/dashboard');
-		return;
-	}
-	res.render('signUp');
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+  res.render('signUp');
 });
 
 module.exports = router;
