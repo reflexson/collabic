497155e805a456ecf594712f0bd95506b5a3cdf6
supragirl100@gmail.com <supragirl100@gmail.com>
2023-08-24 22:55:52 -0400

    index.js

--- models/index.js
+++ models/index.js
@@ -1,10 +1,6 @@
 const User = require('./user');
-<<<<<<< HEAD
 const Project = require('./project');
 const Song = require('./song');
-=======
-const Blog = require('./song');
->>>>>>> e8688c0c56092ce4345a98d9879ca6a2f9499eec
 const Comment = require('./comment');
 
 User.hasMany(Project, {
