282fada328f39ae45b0b102c8f6d00a4a8b59be1
abstractl0gic <zwlyons@gmail.com>
2023-08-29 20:44:56 -0500

    autodetect link type + comment out radio buttons

--- public/js/project.js
+++ public/js/project.js
@@ -11,47 +11,93 @@ const newSongHandler = async (event) => {
 	
  
 
- // determine the link type based on the radio button selection
- const link_type = gdRadio.checked ? 'gd' : 'db';
- // logic to HOPEFULLY determine the type of link
- let processedUrl = raw_url;
- if (link_type === 'db') {
-   // process the dropbox URL (if needed)
-   processedUrl = raw_url.slice(0,-4) + "raw=1";
- } else if (link_type === 'gd') {
-   const gdArray = raw_url.split('/');
-   processedUrl = `https://docs.google.com/uc?export=open&id=${gdArray[5]}`;
- }
+	// autodetect the link type based on URL format
+	let link_type = 'unknown';
+	let processedUrl = raw_url;
+	
+	if (raw_url.includes('dropbox.com')) {
+		link_type = 'db';
+		// Replace ?dl=0 with ?dl=1 for direct download link
+		processedUrl = raw_url.replace('?dl=0', '?dl=1');
+	} else if (raw_url.includes('google.com') && raw_url.includes('/file/d/')) {
+		link_type = 'gd';
+		const gdArray = raw_url.split('/');
+		processedUrl = `https://docs.google.com/uc?export=open&id=${gdArray[5]}`;
+	}
+		if (song_name && song_description) {
+			try {
+				const response = await fetch('/api/songs', {
+					method: 'POST',
+					body: JSON.stringify({
+						song_name,
+						song_description,
+						project_id: projectId,
+						song_url: processedUrl,
+						link_type,
+					}),
+					headers: {
+						'Content-Type': 'application/json',
+					},
+				});
+	
+				if (response.ok) {
+					document.location.replace(`/project/${projectId}`);
+				} else {
+					alert('Please fill out all fields');
+				}
+			} catch (error) {
+				console.error(error);
+				// more error handling
+			}
+		}
+	};
+	
+	document.querySelector('.newSongForm').addEventListener('submit', newSongHandler);
 
- if (song_name && song_description) {
-   try {
-	 const response = await fetch('/api/songs', {
-	   method: 'POST',
-	   body: JSON.stringify({
-		 song_name,
-		 song_description,
-		 project_id: projectId,
-		 song_url: processedUrl,
-		 link_type, 
-	   }),
-	   headers: {
-		 'Content-Type': 'application/json',
-	   },
-	 });
 
-	 if (response.ok) {
-	   document.location.replace(`/project/${projectId}`);
-	 } else {
-	   alert('Please fill out all fields');
-	 }
-   } catch (error) {
-	 console.error(error);
-	 // error stuff
-   }
- }
-};
+	// OLD METHOD	
 
-document.querySelector('.newSongForm').addEventListener('submit', newSongHandler);
+//  // determine the link type based on the radio button selection
+//  const link_type = gdRadio.checked ? 'gd' : 'db';
+//  // logic to HOPEFULLY determine the type of link
+//  let processedUrl = raw_url;
+//  if (link_type === 'db') {
+//    // process the dropbox URL (if needed)
+//    processedUrl = raw_url.slice(0,-4) + "raw=1";
+//  } else if (link_type === 'gd') {
+//    const gdArray = raw_url.split('/');
+//    processedUrl = `https://docs.google.com/uc?export=open&id=${gdArray[5]}`;
+//  }
+
+//  if (song_name && song_description) {
+//    try {
+// 	 const response = await fetch('/api/songs', {
+// 	   method: 'POST',
+// 	   body: JSON.stringify({
+// 		 song_name,
+// 		 song_description,
+// 		 project_id: projectId,
+// 		 song_url: processedUrl,
+// 		 link_type, 
+// 	   }),
+// 	   headers: {
+// 		 'Content-Type': 'application/json',
+// 	   },
+// 	 });
+
+// 	 if (response.ok) {
+// 	   document.location.replace(`/project/${projectId}`);
+// 	 } else {
+// 	   alert('Please fill out all fields');
+// 	 }
+//    } catch (error) {
+// 	 console.error(error);
+// 	 // error stuff
+//    }
+//  }
+// };
+
+// document.querySelector('.newSongForm').addEventListener('submit', newSongHandler);
 
 
 	
